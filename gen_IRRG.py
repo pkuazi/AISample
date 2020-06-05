6c31e7ab3cf484f9da562df640654050fd353b8a
zhenbing <zjh@cnic.cn>
2020-06-05 09:21:25 +0800

    ir r g bands composite with B5 in filename instead of endswith

--- gen_IRRG.py
+++ gen_IRRG.py
@@ -2,28 +2,24 @@ import os
 
 ROOT_PATH = '/mnt/rsimages/lulc/AISample'
 irrg_path = os.path.join(ROOT_PATH,'BANDS_IRRG')
+
+# imageid_path = os.path.join(ROOT_PATH,'IMAGE')
+# imageids_file = os.path.join(imageid_path,'imageids.csv')
+
 image_path = os.path.join(ROOT_PATH,'BANDS_ALL')
 files = os.listdir(image_path)
-file_ends = {'LM1':{'IR' :'_B6.TIF', 'R' : '_B5.TIF', 'G' : '_B4.TIF'},'LM2':{'IR' :'_B6.TIF', 'R' : '_B5.TIF', 'G' : '_B4.TIF'},'LM3':{'IR' :'_B6.TIF', 'R' : '_B5.TIF', 'G' : '_B4.TIF'}, 'LT5':{'IR':'_B4.TIF', 'R' : '_B3.TIF', 'G' : '_B2.TIF'}, 'LE7':{'IR' :'_B40.TIF', 'R' : '_B30.TIF', 'G' :'_B20.TIF'}, 'LC8':{'IR' : '_B5.TIF', 'R' : '_B4.TIF', 'G' : '_B3.TIF'}}
+file_with = {'LM1':{'IR' :'_B6', 'R' : '_B5', 'G' : '_B4'},'LM2':{'IR' :'_B6', 'R' : '_B5', 'G' : '_B4'},'LM3':{'IR' :'_B6', 'R' : '_B5', 'G' : '_B4'}, 'LT5':{'IR':'_B4', 'R' : '_B3', 'G' : '_B2'}, 'LE7':{'IR' :'_B4', 'R' : '_B3', 'G' :'_B2'}, 'LC8':{'IR' : '_B5', 'R' : '_B4', 'G' : '_B3'}}
+# LT5 may also ends with B50.TIF
 for file in files:
     file_path = os.path.join(image_path,file)
-    IR_END = file_ends[file[0:3]]['IR']
-    R_END = file_ends[file[0:3]]['R']
-    G_END = file_ends[file[0:3]]['G']
-        
-    bandir = os.path.join(file_path, file+IR_END)
-    bandr = os.path.join(file_path, file+R_END)
-    bandg = os.path.join(file_path, file+G_END)
-
-    if not os.path.exists(bandir):
-        bands = os.listdir(file_path)
-        for bandf in bands:
-            if bandf.endswith(IR_END):
-                bandir = os.path.join(file_path, bandf)
-            elif bandf.endswith(R_END):
-                bandr = os.path.join(file_path, bandf)
-            elif bandf.endswith(G_END):
-                bandg = os.path.join(file_path, bandf)
+    bands = os.listdir(file_path)
+    for bandf in bands:
+        if file_with[file[0:3]]['IR'] in bandf:
+            bandir = os.path.join(file_path, bandf)
+        elif file_with[file[0:3]]['R'] in bandf:
+            bandr = os.path.join(file_path, bandf)
+        elif file_with[file[0:3]]['G'] in bandf:
+            bandg = os.path.join(file_path, bandf)
                         
     outfile = os.path.join(irrg_path,file+'_IRRG.TIF')
     cmd = 'gdal_merge.py -tap -separate -o  %s -of GTiff %s %s %s'%(outfile, bandir, bandr, bandg)
