75a4eec63c5353084a598069e9defcdad3db28be
zhenbing <zjh@cnic.cn>
2020-06-16 10:16:02 +0800

    check tiling dataset

--- check.py
+++ check.py
@@ -4,13 +4,18 @@ irrg = '/mnt/rsimages/lulc/AISample/TILE_IRRG/'
 dem = '/mnt/rsimages/lulc/AISample/TILE_DEM/'
 
 files=os.listdir(gt)
+gt_num=0
+irrg_num=0
+dem_num=0
 for file in files:
+    gt_num+=1
 #    print(file)
     start = file[0:13]
     
     ifiles = os.listdir(irrg)
     found = 0    
     for ifile in ifiles:
+        irrg_num+=1
         if ifile.startswith(start):
             found=1
  #           print('find it!',ifile)
@@ -21,9 +26,13 @@ for file in files:
     dfiles = os.listdir(dem)
     found = 0
     for dfile in dfiles:
+        dem_num+=1
         if dfile.startswith(start):
             found=1
  #           print('find it!',ifile)
             continue
     if found==0:
         print('no dem match for ',file)
+print('the number of  gt tiles is ', gt_num)
+print('the number of  irrg tiles is ', gt_num)
+print('the number of  dem tiles is ', gt_num)
